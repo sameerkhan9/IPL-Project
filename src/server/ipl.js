23176dc55f8eb60c9aeb690412ffc7f5ecd273fa
SameerKhan17 <sameer.khan.17@mountblue.tech>
2021-08-23 19:48:15 +0530

    Add ipl.js

---
+++ src/server/ipl.js
@@ -0,0 +1,14 @@
+const matchesPerYear = require('./matchesPerYear.js');
+const matchesWonPerTeamPerYear = require('./matchesWonPerTeamPerYear.js');
+const extraRunsPerTeam2016 = require('./extraRunsPerTeam2016');
+const top10EconomicalBowlers = require('./top10EconomicalBowlers.js');
+
+
+module.exports = {
+    top10EconomicalBowlers,
+    matchesPerYear,
+    matchesWonPerTeamPerYear,
+    extraRunsPerTeam2016
+
+};
+
