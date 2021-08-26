cc17e0d4c5c896abf12250cf7251fc53fcee62a4
SameerKhan17 <sameer.khan.17@mountblue.tech>
2021-08-26 13:28:14 +0530

    Import timesTeamWonTossAndMatch module  in ipl.js and export it with other modules

--- src/server/ipl.js
+++ src/server/ipl.js
@@ -2,13 +2,16 @@ const matchesPerYear = require('./matchesPerYear.js');
 const matchesWonPerTeamPerYear = require('./matchesWonPerTeamPerYear.js');
 const extraRunsPerTeam2016 = require('./extraRunsPerTeam2016');
 const top10EconomicalBowlers = require('./top10EconomicalBowlers.js');
+const timesTeamWonTossAndMatch = require('./timesTeamWonTossAndMatch.js');
+
 
 
 module.exports = {
     top10EconomicalBowlers,
     matchesPerYear,
     matchesWonPerTeamPerYear,
-    extraRunsPerTeam2016
+    extraRunsPerTeam2016,
+    timesTeamWonTossAndMatch
 
 };
 
